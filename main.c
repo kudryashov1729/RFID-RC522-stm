a062a55fbb644e7d94731d428f0378387fa98d23
Mikheev <ng.mikheev@ya.ru>
2020-05-04 23:42:27 +0300

    Missed dependencies added to project. MFRC init uncommented. Card check added.

--- main.c
+++ main.c
@@ -11,8 +11,10 @@
         //  AF5 SPI1_SCK  PA5
         //  AF5 SPI1_MISO PA6
         //  AF5 SPI1_MOSI PA7
-  uint8_t addr1;
-  volatile uint8_t val_1 = 1;
+uint8_t addr1, card_id[5];
+volatile uint8_t val_1 = 1, card_is_here = 0;
+  
+  
 void main()
 {
   //Initialize MFRC522 RFID
@@ -23,7 +25,17 @@ void main()
   addr1 = 0x35;
   val_1 = TM_MFRC522_ReadRegister( addr1);
   
-
+  
+  
+  while (TM_MFRC522_Check(card_id) != MI_OK) {
+    card_is_here = 0;
+    addr1 = 0x37;
+    val_1 = TM_MFRC522_ReadRegister( addr1);
+    addr1 = 0x35;
+    val_1 = TM_MFRC522_ReadRegister( addr1);
+  }
+  card_is_here = 1;
+  
   while(1){
     }
 };
